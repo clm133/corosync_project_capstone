c6b82287bf686bb6a776bd656aa23f44f6a61851
charlie <charlie@charlie.home>
2016-10-18 09:36:26 -0400

    beginning to actually add cmap functions...

--- corosync-client/src/client_cmap.c
+++ corosync-client/src/client_cmap.c
@@ -16,6 +16,43 @@ int cmap_close(cmap_handle_t conn)
 	return 0;
 }
 
+int print_membership()
+{
+	struct node_info{
+		uint32_t node_id;
+        char node_name[128];
+    }nodes[4]; //only 4 for now. can change to support more in the future
+	int err;
+	cmap_handle_t conn; 
+	cmap_iter_handle_t iter_h;
+	uint32_t node_id ;
+	
+	//initialize connection and iterator
+	err = cmap_init(conn);
+	if(err != 0){
+		printf("Could not establish connection\n");
+		return err;
+	}
+	err = cmap_iter_init(conn, "nodelist.node", &iter_h);
+	if(err != 0){
+		printf("Could not establish cmap iterator\n");
+		return err;
+	}
+	while(1){
+		char key_name[32];
+        size_t val_len = 0;
+        cmap_value_types_t val_type;
+        err = cmap_iter_next(conn, iter_h, key_name, &val_len, &val_type);
+		if (err != 0) {
+            break;
+        }
+		printf("This is a key_name: %s\n", key_name);
+	}
+	(void)cmap_iter_finalize (conn, iter_h);
+	err = cmap_close(conn);
+	return err;
+}
+
 int add_node(char *addr)
 {
 	int err;
